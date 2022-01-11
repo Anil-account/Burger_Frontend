4424d7f9b01c9f90f5678b28c138e8fd896dbedd
Sanjaya Thapa <thapaanil62@gmail.com>
2022-01-11 12:59:29 +0545

    creating user page

---
+++ happy-burger/src/User/Login.js
@@ -0,0 +1,13 @@
+import { Component } from "react/cjs/react.production.min";
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is Login page</p>
+            </div>
+        )
+    }
+}
+
+export default Login
---
+++ happy-burger/src/User/Signup.js
@@ -0,0 +1,13 @@
+import { Component } from "react/cjs/react.production.min";
+
+class Signup extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is Signup page</p>
+            </div>
+        )
+    }
+}
+
+export default Signup
