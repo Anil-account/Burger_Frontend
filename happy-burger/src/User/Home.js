ee0b9f1d5dc83caff862b6a06cdd27afa520b45d
Sanjaya Thapa <thapaanil62@gmail.com>
2022-01-12 13:00:25 +0545

    hpme page

---
+++ happy-burger/src/User/Home.js
@@ -0,0 +1,13 @@
+import { Component } from "react/cjs/react.production.min";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is home page</p>
+            </div>
+        )
+    }
+}
+
+export default Home
