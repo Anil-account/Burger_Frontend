2673a4907b39ee730c5d96d37f6e413472f5ce56
Sanjaya Thapa <thapaanil62@gmail.com>
2022-02-05 19:05:51 +0545

    update

--- happy-burger/src/Templates/Middleware/Mid.js
+++ happy-burger/src/Templates/Middleware/Mid.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Component } from "react/cjs/react.development";
+import Home from "../User/Home";
 import Login from "../User/Login";
 import Logout from "../User/Logout";
 import Menu from "../User/Menu";
 import ProductDetail from "../User/productDetail";
 import Signup from "../User/Signup";
+import ViewCart from "../User/viewCart";
 import ViewProduct from "../User/ViewProduct";
 
 class Mid extends Component {
@@ -12,12 +14,14 @@ class Mid extends Component {
     return (
       <div>
         <Routes>
+          <Route path="/home" element={<Home/>}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Signup />}></Route>
           <Route  path="/logout/*" element={<Logout/>}></Route>
           <Route path="/menu" element={<Menu/>}></Route>
           <Route path="/product" element={<ViewProduct/>}></Route>
           <Route path="/viewdetail" element={<ProductDetail/>}></Route>
+          <Route path="/viewcart" element={<ViewCart/>}></Route>
         </Routes>
       </div>
     );
