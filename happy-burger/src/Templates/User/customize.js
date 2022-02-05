c65179cc4383ed5c25cd0292c1308a3bdd82bf86
Sanjaya Thapa <thapaanil62@gmail.com>
2022-02-05 22:23:10 +0545

    trying to add customization

---
+++ happy-burger/src/Templates/User/customize.js
@@ -0,0 +1,193 @@
+const Customize = () => {
+  return (
+    <div className="bg-light pt-3 pb-4 ">
+      <div className="container ">
+        <div className="col-6  position-relative mx-auto ">
+          <h1 className="text-align-center text-primary fs-1 text-color-customization">Customize your desired burger</h1>
+          <br className="p-3"></br>
+          <form className="p-4 card">
+
+            <div class="form-group row pb-4">
+              <label for="inputEmail3" class="col-sm-2 col-form-label">
+                Bun
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="inputEmail3"
+                  placeholder="Email"
+                ></input>
+              </div>
+            </div>
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Patty
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Cheese
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Mushroom
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Egg
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Tomato
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Onion
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Pickle
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Lettuce
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Mayonnaise
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Ketchu[]
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Baycon
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>{" "}
+            <div class="form-group row pb-4">
+              <label for="inputPassword3" class="col-sm-2 col-form-label">
+                Patty
+              </label>
+              <div class="col-sm-10">
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword3"
+                  placeholder="Password"
+                ></input>
+              </div>
+            </div>
+            <div class="form-group row pb-4">
+              <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">
+                  Sign in
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Customize;
