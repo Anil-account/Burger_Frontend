50bdc831fbabc7936efa958db12217a20a1131c9
Sanjaya Thapa <thapaanil62@gmail.com>
2022-02-12 16:16:13 +0545

    new feature added

---
+++ src/components/main/section/Section2.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Section2() {
+    return (
+        <div className="banner-2">
+            <div className="banner-2-container">
+                <div className="banner-2-image" style={{backgroundImage:'url(https://png.pngtree.com/thumb_back/fw800/back_our/20190621/ourmid/pngtree-thickened-big-burger-promotion-simple-banner-image_196612.jpg'}}></div>
+                <div className="banner-2-text text-width-bold" style={{color:"orangered"}}>
+                    <h3 style={{}}>Nepal Made</h3>
+                    <p style={{fontSize:'3rem', fontWeight:'bold'}}>Hot sauce burger</p>
+                    <p style={{fontSize:'1.5rem', fontWeight:'bold'}}>See More</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Section2;
